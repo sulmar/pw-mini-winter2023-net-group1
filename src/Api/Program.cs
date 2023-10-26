da20a4869be1232e3f57b9cb4ca3ff11f67bc18c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-26 15:56:42 +0200

    Added Swagger

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -6,9 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IUserRepository, FakeUserRepository>();
 
 var app = builder.Build();
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
